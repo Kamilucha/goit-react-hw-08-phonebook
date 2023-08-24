1ffa92bcfccd952c21e6f0d18ef662b5656f3f8b
Kamilucha <brnkamila2@gmail.com>
2023-08-24 17:48:41 +0200

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>
-        Task manager welcome page{' '}
+       ляляля
         <span role="img" aria-label="Greeting icon">
           💁‍♀️
         </span>
